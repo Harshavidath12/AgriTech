fc67485555f46113a9983e9d4dfee75c8a4341db
Vidath Harshitha <68719470+Harshavidath12@users.noreply.github.com>
2026-06-10 17:11:28 +0530

    Add Notification model

---
+++ Backend/models/Notification.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipientId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ['Booking_Request', 'Booking_Approved', 'Payment_Received'],
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+notificationSchema.index({ recipientId: 1, createdAt: -1 });
+
+module.exports = mongoose.model('Notification', notificationSchema);
